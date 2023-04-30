a6587dd20f498d4eb18a02cafaaae526b7a9874a
lucas <stempest@163.com>
2023-04-30 22:58:22 +0800

    隐藏lni/slk避免意外点击，尝试code生成ui。

--- scripts/main.lua
+++ scripts/main.lua
@@ -4,14 +4,36 @@ _F.FogEnable(false)
 _F.FogMaskEnable(false)
 
 _U.toc("UI\\path.toc")
+local console = require 'jass.console'
+console.enable = true
 
 -- _J.DzFrameHideInterface()
 -- _J.DzFrameEditBlackBorders(0, 0)
 
+
+
 function fun()
     -- local parent = _U.createFrame('BACKDROP', "Demo_BorderBack").position(0.04, 0.2).show().frameHandle
     -- _U.createFrame('BUTTON', "Demo_Button", parent).position(0.04, 0.2).show()
-    _U.create("BagFrame").position(0.04, 0.2).show()
+    local bagId = 99
+    _U.create("BagFrame", bagId).position(0.04, 0.2).show()
+
+    local leftItems = {
+        [1] = { icon = 'ReplaceableTextures\\CommandButtonsDisabled\\DISBTN3M1.blp' },
+        [2] = { icon = 'ReplaceableTextures\\CommandButtonsDisabled\\DISBTN3M1.blp' },
+        [3] = { icon = 'ReplaceableTextures\\CommandButtonsDisabled\\DISBTN3M1.blp' },
+        [4] = { icon = 'ReplaceableTextures\\CommandButtonsDisabled\\DISBTN3M1.blp' },
+        [5] = { icon = 'ReplaceableTextures\\CommandButtonsDisabled\\DISBTN3M1.blp' }
+    }
+    local _parent = _U.find("LeftFrame", bagId)
+    console.write(bagId, _parent)
+
+    for i, v in ipairs(leftItems) do
+        local position = -0.04 * i
+        console.write(i, v.icon, position)
+        _U.create("TestItem", i, _parent).position(0, 0).img(v.icon).show()
+        -- _J.DzFrameSetParent(now.frameHandle, _parent)
+    end
 end
 
 fun()
